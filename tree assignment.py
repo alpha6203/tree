14df1262cc5925023b469ef4d046466f9e760c4c
Abhishek Ranjan <95959919+alpha6203@users.noreply.github.com>
2023-05-05 15:44:00 +0530

    Add files via upload

---
+++ tree assignment.py
@@ -0,0 +1,28 @@
+class newNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+ 
+def printOddNodes(root, isOdd = True):
+     
+    # If empty tree
+    if (root == None):
+        return
+ 
+    # If current node is of odd level
+    if (isOdd):
+        print(root.data, end = " ")
+ 
+    # Recur for children with isOdd
+    # switched.
+    printOddNodes(root.left, not isOdd)
+    printOddNodes(root.right, not isOdd)
+ 
+# Driver code
+if __name__ == '__main__':
+    root = newNode(1)
+    root.left = newNode(2)
+    root.right = newNode(3)
+    root.left.left = newNode(4)
+    root.left.right = newNode(5)
+    printOddNodes(root)
